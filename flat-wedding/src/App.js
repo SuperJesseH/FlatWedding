8cba6bcb1c53094d4fefa7cc352def67414a6378
SuperJesseH <therealjhorwitz@gmail.com>
2018-06-18 16:37:13 -0400

    started working on the nav and app state

--- flat-wedding/src/App.js
+++ flat-wedding/src/App.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 import NavBar from './components/NavBar'
 import MainBody from './components/MainBody'
-import CurrentForm from './components/CurrentForm'
+import Form from './components/Form'
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {currentForm:"login"}
+  }
+
+  setCurrentForm = (formName) =>{
+    this.setState({
+      currentForm:`${formName}`
+    })
+  }
+
   render() {
+    const buttons = ["fee", "fye", "foe", "fum"]
+    console.log(this.state);
     return (
       <div className="App">
-        <NavBar className="NavBar"></NavBar>
-        <CurrentForm className="CurrentForm"></CurrentForm>
+        <NavBar className="NavBar" setCurrentForm={this.setCurrentForm} buttons={buttons}></NavBar>
+        <Form className="Form"></Form>
         <MainBody className="MainBody"></MainBody>
       </div>
     );
--- flat-wedding/src/components/NavBar.js
+++ flat-wedding/src/components/NavBar.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 
 
-export default class NavBar extends Component{
+const NavBar = (props)=>{
 
-  render(){
-    return(
-      <div>Hi im the NavBar</div>
-    )
+
+  const handleButton = (e) =>{
+    props.setCurrentForm(e.currentTarget.innerHTML)
   }
+
+
+    return(
+      <div>
+        <div className='NavText'>
+          <h1>melanie + jesse</h1>
+          <h2>13 april 2019</h2>
+          <h2>madison, wi</h2>
+        </div>
+
+        <div className='NavButtonRow'>
+            {props.buttons.map((aButton)=>
+              <button key={aButton} onClick={(aButton)=>handleButton(aButton)}>{aButton}</button>)}
+        </div>
+      </div>
+    )
 }
+
+export default NavBar
